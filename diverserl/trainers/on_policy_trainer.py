aad98638da4caf8fb3205579ae1cd85a6ea7d9eb
moripiri <121962734+moripiri@users.noreply.github.com>
2023-12-06 02:49:35 +0000

    on_policy_trainer-wip

--- diverserl/trainers/on_policy_trainer.py
+++ diverserl/trainers/on_policy_trainer.py
@@ -1,19 +1,15 @@
 import gymnasium as gym
 
 from diverserl.algos.deep_rl.base import DeepRL
-from diverserl.trainers import DeepRLTrainer
 from diverserl.trainers.base import Trainer
 
 
-class OnPolicyTrainer(DeepRLTrainer):
+class OnPolicyTrainer(Trainer):
     def __init__(
         self,
         algo: DeepRL,
         env: gym.Env,
         eval_env: gym.Env,
-        training_start: int = 256,
-        training_num: int = 1,
-        train_type: str = "online",
         max_step: int = 10000,
         do_eval: bool = True,
         eval_every: int = 1000,
@@ -23,11 +19,15 @@ class OnPolicyTrainer(DeepRLTrainer):
             algo=algo,
             env=env,
             eval_env=eval_env,
-            training_start=training_start,
-            training_num=training_num,
-            train_type=train_type,
-            max_step=max_step,
+            total=max_step,
             do_eval=do_eval,
             eval_every=eval_every,
             eval_ep=eval_ep,
         )
+        
+        
+    def evaluate(self) -> None:
+        pass
+    
+    def run(self) -> None:
+        pass
